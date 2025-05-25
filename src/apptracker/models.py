f789b18903b7b0d4a5fa0dfa434c6df3ee6b064e
Ian Valeta <valeta.iac@gmail.com>
2025-05-24 22:54:51 -0400

    added code

--- src/apptracker/models.py
+++ src/apptracker/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# class Application(models.Model):
+    
+#     INACTIVE    = "INA"
+#     APPLIED     = "APL"
+#     INTERVIEWED = "INT"
+#     OFFERED     = "OFR"
+#     REJECTED    = "RJT"
+#     STATUS_CHOICES = [
+#         (INACTIVE,    "Inactive"),
+#         (APPLIED,     "Applied"),
+#         (INTERVIEWED, "Interviewed"),
+#         (OFFERED,     "Offered"),
+#         (REJECTED,    "Rejected")
+#     ]
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#         related_name='applications'
+#     )
+#     company  = models.CharField(blank=True, null=False, max_length=200)
+#     position = models.CharField(blank=True, null=False, max_length=200)
+#     location = models.CharField(blank=True, null=False, max_length=200)
+#     pay      = models.CharField(blank=True, null=False, max_length=200)
+#     status   = models.CharField(blank=True, null=False, choices=STATUS_CHOICES, default=INACTIVE, max_length=3)
+#     url      = models.URLField (blank=True, null=False, max_length=200)
+#     notes    = models.TextField(blank=True, null=False)
--- src/apptracker/views.py
+++ src/apptracker/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+# from .forms import ApplicationForm
+# from .models import Application
 
 def app_tracker(request):
-    return render(request, "apptracker/apptracker.html")
+    return render(request, 'apptracker/apptracker.html')
