fca795995aa44d8991d1f1194af54f7c957698bc
Ian Valeta <valeta.iac@gmail.com>
2025-06-10 22:49:46 -0400

    deleted job app tracker code

--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -1,17 +1,7 @@
-import Header from "@/components/ui/Header";
-
-export default function Tracker() {
+export default function JobApplicationTracker() {
     return (
         <div>
-            <Header>Job Application Tracker</Header>
-            <div className="flex w-full justify-between space-x-2">
-                <div className="flex w-1/6 justify-center px-4 py-2 bg-accent rounded-lg text-background">Company</div>
-                <div className="flex w-1/6 justify-center px-4 py-2 bg-accent rounded-lg text-background">Position</div>
-                <div className="flex w-1/6 justify-center px-4 py-2 bg-accent rounded-lg text-background">Location</div>
-                <div className="flex w-1/6 justify-center px-4 py-2 bg-accent rounded-lg text-background">Compensation</div>
-                <div className="flex w-1/6 justify-center px-4 py-2 bg-accent rounded-lg text-background">Application Date</div>
-                <div className="flex w-1/6 justify-center px-4 py-2 bg-accent rounded-lg text-background">Status</div>
-            </div>
+            <h1>Job Application Tracker</h1>
         </div>
     );
 }
