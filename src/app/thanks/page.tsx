3c334cbefff2710eef4c76d14914d7b7bfd5c077
Ian Valeta <valeta.iac@gmail.com>
2025-07-06 17:02:02 -0400

    clean slate

--- src/app/thanks/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Thanks() {
-    return (
-        <div>
-            <h1 className="mt-24 text-2xl ">Thanks for signing up!</h1>
-            <p className="mt-3 text-xl text-accent">We will send you updates and exclusive early access to our tools.</p>
-        </div>
-    )
-}
--- src/components/ui/Heading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Heading({ children }: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <h1 className="text-6xl my-4 w-full">
-            {children}
-        </h1>
-    );
-}
